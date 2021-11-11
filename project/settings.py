277221543a4fde9272b6ad48b141f844e4105125
EnasMohamed <49351823+enasmohmed@users.noreply.github.com>
2021-11-11 17:28:49 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-menxy4(+acyeh!r6oosdjz*+uin#%4q-sto=k0os_)a%0czk5z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['businesselite.herokuapp.com']
 
 
 # Application definition
